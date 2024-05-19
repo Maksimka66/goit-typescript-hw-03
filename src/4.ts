7379b92473b06565dc437a153ceb7f9d1c8f7f64
Maksimka66 <maxfese@gmail.com>
2024-05-19 20:58:53 +0300

    third work done

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,53 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public key: Key;
+  public tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (!this.door) {
+      return;
+    }
+
+    this.tenants.push(person);
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
+console.log(key.getSignature());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +55,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
